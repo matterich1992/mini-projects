cdae00953be43c05fb252ec4e592083b9b9522ce
Matthew Erich <matthewerich1992@gmail.com>
2016-08-24 22:53:07 -0700

    Added Youtube API
    Formatting AJAX request to fetch Youtube Video data and display onto a
    search page. ID, Date, Title, Etc…

--- Youtube Data API/js/script.js
+++ Youtube Data API/js/script.js
@@ -1,8 +1,8 @@
 // Searchbar Handler
-$(document).ready(function(){
+$(function(){
 	var searchField = $('#query');
 	var icon = $('#search-btn');
-
+	
 	// Focus Event Handler
 	$(searchField).on('focus', function(){
 		$(this).animate({
@@ -12,7 +12,7 @@ $(document).ready(function(){
 			right: '10px'
 		}, 400);
 	});
-
+	
 	// Blur Event Handler
 	$(searchField).on('blur', function(){
 		if(searchField.val() == ''){
@@ -24,21 +24,21 @@ $(document).ready(function(){
 			},400, function(){});
 		}
 	});
-
+	
 	$('#search-form').submit(function(e){
 		e.preventDefault();
 	});
-});
+})
 
 
 function search(){
 	// Clear Results
 	$('#results').html('');
 	$('#buttons').html('');
-
+	
 	// Get Form Input
 	q = $('#query').val();
-
+	
 	// Run GET Request on API
 	$.get(
 		"https://www.googleapis.com/youtube/v3/search?key=AIzaSyA4beXgE06wacapByCnJUmq-tVtSI4ipCY",{
@@ -48,15 +48,45 @@ function search(){
 			function(data){
 				var nextPageToken = data.nextPageToken;
 				var prevPageToken = data.prevPageToken;
-
+				
 				// Log Data
 				console.log(data);
-
-				$.each(data.items,function(i, item){
-					var output=getOutput(item);
-					//Display the Results
-					$('#results').append(output);
-				});
-			}
+				
+			$.each(data.items, function(i, item){
+				//Get the Output
+				var output = getOutput(item);
+			
+			//Display results
+			$('#results').append(output);
+			});
+		}
 	);
 }
+
+//Build Output
+
+function getOutput(item){
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+
+// Build the Output String of HTML for each list item
+	var output = '<li>' +
+	'<div class="list-left">' +
+	'<img src="'+thumb+'">' +
+	'</div>' +
+	'<div class="list-right">' +
+	'<h3>'+title+'</h3>' +
+	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
+	'<p>'+description+'</p>' +
+	'</div>' +
+	'</li>' +
+	'<div class="clearfix"></div>' +
+	'';
+	
+	return output;
+}
+
