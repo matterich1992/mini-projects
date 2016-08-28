45297324bb8cf39ce9556b63708e24e0799ff051
Matthew Erich <matthewerich1992@gmail.com>
2016-08-27 19:52:39 -0700

    Youtube MiniProject
    temp to implement fancy box while using using youtube API.

--- Youtube Data API/js/script.js
+++ Youtube Data API/js/script.js
@@ -1,5 +1,5 @@
 // Searchbar Handler
-$(function(){
+$(document).ready(function(){
 	var searchField = $('#query');
 	var icon = $('#search-btn');
 	
@@ -59,12 +59,99 @@ function search(){
 			//Display results
 			$('#results').append(output);
 			});
+
+			var buttons = getButtons(prevPageToken,nextPageToken);
+
+			$('#buttons').append(buttons);
 		}
 	);
 }
 
-//Build Output
+//Next page Function
 
+function nextPage(){
+var token = $('#next-button').data('token');
+var q = $('#next-button').data('query');
+// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search?key=AIzaSyA4beXgE06wacapByCnJUmq-tVtSI4ipCY",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type:'video'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+			$.each(data.items, function(i, item){
+				//Get the Output
+				var output = getOutput(item);
+			
+			//Display results
+			$('#results').append(output);
+			});
+
+			var buttons = getButtons(prevPageToken,nextPageToken);
+
+			$('#buttons').append(buttons);
+		}
+	);
+
+}
+
+//Previous Page function
+
+function prevPage(){
+	var token = $('#prev-button').data('token');
+var q = $('#prev-button').data('query');
+// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search?key=AIzaSyA4beXgE06wacapByCnJUmq-tVtSI4ipCY",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type:'video'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+			$.each(data.items, function(i, item){
+				//Get the Output
+				var output = getOutput(item);
+			
+			//Display results
+			$('#results').append(output);
+			});
+
+			var buttons = getButtons(prevPageToken,nextPageToken);
+
+			$('#buttons').append(buttons);
+		}
+	);
+
+}
+
+// Build Output
 function getOutput(item){
 	var videoId = item.id.videoId;
 	var title = item.snippet.title;
@@ -72,14 +159,14 @@ function getOutput(item){
 	var thumb = item.snippet.thumbnails.high.url;
 	var channelTitle = item.snippet.channelTitle;
 	var videoDate = item.snippet.publishedAt;
-
-// Build the Output String of HTML for each list item
+	
+	// Build Output String
 	var output = '<li>' +
 	'<div class="list-left">' +
 	'<img src="'+thumb+'">' +
 	'</div>' +
 	'<div class="list-right">' +
-	'<h3>'+title+'</h3>' +
+	'<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">'+title+'</a></h3>' +
 	'<small>By <span class="cTitle">'+channelTitle+'</span> on '+videoDate+'</small>' +
 	'<p>'+description+'</p>' +
 	'</div>' +
@@ -89,4 +176,24 @@ function getOutput(item){
 	
 	return output;
 }
+
+// Build the buttons
+function getButtons(prevPageToken, nextPageToken){
+	if(!prevPageToken){
+		var btnoutput = '<div class="button-container">'+'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">'+
+		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+		'onclick="prevPage();">Prev Page</button>' +
+		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+		'onclick="nextPage();">Next Page</button></div>';
+	}
+	
+	return btnoutput;
+}
+
+
+
+
 
